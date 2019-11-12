69ae4e4c4a43c0ac0da22cccf4e5f1c7588157c5
Simon Possegger <posseggs@edu.htl-villach.at>
2019-11-12 23:41:00 +0100

    Update CardReader to be same as master

--- CardReader.py
+++ CardReader.py
@@ -3,165 +3,113 @@ Created by Simon Possegger on 12.11.2019 as part of the Infineon Hackathon.
 This Class asynchronously reads from the card reader
 and provides the needed functions to interact with the Infineon Blockchain Security 2Go cards.
 '''
+
 import blocksec2go
-import threading
 import hashlib
 import os
-from time import sleep
-from blocksec2go.comm import observer
 
-class CardReader(object):
 
-    cardmonitor = None
-    cardobserver = None
-    #last_pub = None
-    current_pub = None
+def initReading():
+    reader = get_reader()
+    activate_card(reader)
+    return reader
+
+
+def get_reader():
     reader = None
-
-    def __init__(self, interval=0.5):
-        self.interval = interval
-        thread = threading.Thread(target=self.run, args=(), daemon=True)
-        thread.start()
-
-    def run(self):
+    reader_name = 'Identiv uTrust 3700 F'
+    while reader is None:
         try:
-            self.cardmonitor, self.cardobserver = observer.start()
-            sleep(1)
-            blocksec2go.add_callback(connect=self.connected, disconnect=self.disconnected)
-            try:
-                self.reader = self.get_reader()
-            except:
-                pass
-            while True:
-                sleep(self.interval)
-        except:
-            self.restart()
-
-    def restart(self):
-        if self.cardmonitor is not None and self.cardobserver is not None:
-            observer.stop(self.cardmonitor, self.cardobserver)
-        sleep(self.interval)
-        self.run()
-
-    def get_reader(self):
-        reader = None
-        reader_name = 'Identiv uTrust 3700 F'
-        while reader is None:
-            try:
-                reader = blocksec2go.find_reader(reader_name)
-            except:
-                pass
+            reader = blocksec2go.find_reader(reader_name)
+            print('Found reader %s' % reader_name)
+        except Exception as details:
+            if ('No reader found' == str(details)):
+                print('No card reader found!', end='\r')
+            elif ('No card on reader' == str(details)):
+                print('Found reader, but no card!', end='\r')
+            else:
+                print('ERROR: ' + str(details))
+                raise SystemExit
         return reader
 
-    def activate_card(self):
-        try:
-            blocksec2go.select_app(self.reader)
-        except Exception as details:
-            print('ERROR: %s' % str(details))
-            raise SystemExit
 
-    # TODO:handle new connection -> read card and make db query
-    def connected(self):
-        try:
-            print("Connected\r\n")
-            self.reader = self.get_reader()
-            self.activate_card()
+def activate_card(reader):
+    try:
+        blocksec2go.select_app(reader)
+        print('Found reader and Security 2Go card!')
+    except Exception as details:
+        print('ERROR: %s' % str(details))
+        raise SystemExit
 
-            # if self.current_pub is None and self.last_pub is None:
-            #    self.last_pub = self.current_pub = self.read_public_key(1)
-            #else:
-            #    self.last_pub = self.current_pub # Save current pub before overwriting
-            #    self.current_pub = self.read_public_key(1)
 
-            self.current_pub = self.read_public_key(1)
-            if self.current_pub is not None:
-                print("Public key read: %s with a length of: %d" % (str(self.current_pub.hex()), len(self.current_pub)))
-        except RuntimeError:
-            print("Card is invalid!")
-            sleep(1)
-            self.restart()
-        except:
-            self.restart()
-
-    # TODO:handle disconnect
-    def disconnected(self):
-        print('Disconnected\r\n')
-
-    # Returns true if the card was initiated
-    def initCard(self):
-        try:
-            if self.reader is not None:
-                key_id = blocksec2go.generate_keypair(self.reader)
-                print("Generated key on slot: %s" % str(key_id))
-                return True
-            else:
-                return False
-        except:
+# Returns true if the card was initiated
+def initCard(reader):
+    try:
+        if reader is not None:
+            key_id = blocksec2go.generate_keypair(reader)
+            print("Generated key on slot: %s" % str(key_id))
+            return True
+        else:
             return False
+    except:
+        return False
 
-    def read_public_key(self, key_id):
-        try:
-            if self.reader is not None:
-                if blocksec2go.is_key_valid(self.reader, key_id):  # Check if key is valid
-                    global_counter, counter, key = blocksec2go.get_key_info(self.reader, key_id)
-                    if key is not None:
-                        return key
-                    else:
-                        return None
-                else:
-                    return None
-            else:
-                return None
-        except Exception as details:
-            print('ERROR: ' + str(details))
-            raise SystemExit
 
-    # Returns last read public key
-    def get_Pub_hex(self):
-        if self.current_pub is None:
-            try:
-                self.current_pub = self.read_public_key(1)
-            except:
-                return None
-        try:
-            return self.current_pub.hex()
-        except:
+def read_public_key(reader, key_id):
+    try:
+        if blocksec2go.is_key_valid(reader, key_id):  # Check if key is valid
+            global_counter, counter, key = blocksec2go.get_key_info(reader, key_id)
+            return key
+        else:
             return None
+    except Exception as details:
+        print('ERROR: ' + str(details))
+        raise SystemExit
 
-    def get_Pub(self):
-        if self.current_pub is None:
-            try:
-                self.current_pub = self.read_public_key(1)
-            except:
-                return None
-        return self.current_pub
 
-    def auth(self, pub):
-            return self.verifyPub(pub)
+def auth(reader, pub):
+    return verifyPub(reader, pub)
 
-    def generateSignature(self, hash=None):
-        if hash is None:
-            hash = (hashlib.sha256(b'Hash' + bytearray(os.urandom(10000)))).digest()
-        try:
-            global_counter, counter, signature = blocksec2go.generate_signature(self.reader, 1, hash)
-            return hash, signature
-        except:
-            return None, None
 
-    def verifyPub(self, pub, hash=None, signature=None):
-        # Generate random hash
-        if signature is None:
-            hash, signature = self.generateSignature(hash)
-        try:
-            return blocksec2go.verify_signature(pub, hash, signature)
-        except Exception as ex:
-            print("Verification failed because of error: %s" % str(ex))
-            return False
+def generateSignature(reader, hash=None):
+    if hash is None:
+        hash = (hashlib.sha256(b'Hash' + bytearray(os.urandom(10000)))).digest()
+    try:
+        global_counter, counter, signature = blocksec2go.generate_signature(reader, 1, hash)
+        return hash, signature
+    except:
+        return None, None
 
-'''
-if '__main__' == __name__:
-    CardReader = CardReader()
-    print("Press enter to authenticate")
-    sys.stdin.read(1)
-    print(CardReader.auth(CardReader.get_Pub()))
-'''
+
+def verifyPub(reader, pub, hash=None, signature=None):
+    # Generate random hash
+    if signature is None:
+        hash, signature = generateSignature(reader, hash)
+    try:
+        return blocksec2go.verify_signature(pub, hash, signature)
+    except Exception as ex:
+        print("Verification failed because of error: %s" % str(ex))
+        return False
+
+
+# testing:
+def test():
+    reader = initReading()
+    print("Testing read pub:")
+    pub = read_public_key(reader, 1)
+    if pub is not None:
+        print(pub.hex())
+    else:
+        print("No pub yet... creating one")
+        print("Testing init card")
+        print(initCard(reader))
+        print("Testing read pub again:")
+        pub = read_public_key(reader, 1)
+        print(pub.hex())
+
+    print("Testing auth:")
+    print(auth(reader, read_public_key(reader, 1)))
+
+    print("Testing verify pub with custom hash")
+    hash = (hashlib.sha256(b'OtherHash' + bytearray(os.urandom(10000)))).digest()
+    print(verifyPub(reader, read_public_key(reader, 1), hash))
