1c9904d0d8479b15b88edd512fe3fb5ac6e748bd
Pinterid <david.pinterics@gmail.com>
2019-11-13 01:38:23 +0100

    Add CardReader Class
    Imported the card reader class

--- app/views.py
+++ app/views.py
@@ -10,46 +10,11 @@ import requests
 from flask import render_template, redirect, request
 
 from app import app
+import CardReader
 
 CONNECTED_NODE_ADDRESS = "http://127.0.0.1:8000"
 
 posts = []
-
-def get_reader():
-  reader = None
-  reader_name = 'Identiv uTrust 3700 F'
-  while(reader == None):
-    try:
-      reader = blocksec2go.find_reader(reader_name)
-      print('Found the specified reader and a card!', end='\r')
-    except Exception as details:
-      if('No reader found' == str(details)):
-        print('No card reader found!     ', end='\r')
-      elif('No card on reader' == str(details)):
-        print('Found reader, but no card!', end='\r')
-      else:
-        print('ERROR: ' + str(details))
-        raise SystemExit
-  return reader
-
-def activate_card(reader):
-  try:
-    blocksec2go.select_app(reader)
-    print('Found the specified reader and a Blockchain Security 2Go card!')
-  except Exception as details:
-    print('ERROR: ' + str(details))
-    raise SystemExit
-
-def get_public_key(reader, key_id):
-  try:
-    if(blocksec2go.is_key_valid(reader, key_id)):
-      global_counter, counter, key = blocksec2go.get_key_info(reader, key_id)
-      return key
-    else:
-      raise RuntimeError('Key_id is invalid!')
-  except Exception as details:
-    print('ERROR: ' + str(details))
-    raise SystemExit
 
 def fetch_posts():
     get_chain_address = "{}/chain".format(CONNECTED_NODE_ADDRESS)
@@ -81,23 +46,18 @@ def index():
 def submit_textarea():
     new_tx_address = "{}/new_transaction".format(CONNECTED_NODE_ADDRESS)
 
-    reader = get_reader()
-    activate_card(reader)
+    reader = CardReader.initReading()
 
     publicOne = 'None'
-    publicTwo = get_public_key(reader, 1)
+    publicTwo = CardReader.read_public_key(reader, 1)
 
     json_object = {'publicOne': publicOne,
                    'publicTwo': publicTwo.hex(),
                    'timestamp': time.time()}
 
-    block_string = json.dumps(json_object, sort_keys=True)
-    hash_object = sha256(block_string.encode())
-    hash = hash_object.digest()
+    hash, sign = CardReader.generateSignature(reader, json_object)
 
-    global_counter, counter, signature = blocksec2go.generate_signature(reader, 1, hash)
-
-    if blocksec2go.verify_signature(publicTwo, hash, signature):
+    if CardReader.verifyPub(reader, publicTwo, hash, sign):
         requests.post(new_tx_address,
                       headers={'Content-type': 'application/json'},
                       json=json_object)
